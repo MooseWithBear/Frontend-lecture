f371931492d4e7404c3c0ce5006e7d3bfc354a19
KIM SHINSEONG <moosewithbear@gmail.com>
2022-11-24 09:52:24 +0900

    [1.2.0] 02.JsData start

---
+++ 02_JsData/main.js
@@ -0,0 +1,41 @@
+// console.log(0.1+0.2); // 0.30000000000000004
+// console.log((0.1+0.2).toFixed(2)); // 0.30
+// console.log(typeof((0.1+0.2).toFixed(2))); // string
+// console.log(typeof(Number((0.1+0.2).toFixed(2)))); // number
+
+// let age //undefined
+// setTimeout(() => {
+//     age = 85
+//     console.log(age);
+// }, 1000);
+
+// const user = {
+//     name: "bora",
+//     agee: 22
+// }
+
+// console.log(user.name);
+// console.log(user.agee);
+// console.log(user.email);
+
+// alphabet = ["a","b","c"]; 
+// console.log('alphabet[1]: ', alphabet[1]);
+
+
+// const user2 = new Object(); 
+// user2.name = "bora"; 
+// user2.age=22 
+// console.log(user2)
+
+// function User() {
+//     this.name = "bora"
+//     this.age = 22
+// }
+// const user = new User();
+// console.log(user);
+
+const user = {
+    name:"bora",
+    age:22
+}
+console.log('user: ', user);
---
+++ lecture/02_JsData.js
@@ -0,0 +1,28 @@
+// 001. npm init -y => npm 세팅 시작
+// 002. npm i -D parcel => parcel 번들 설치(개발용)
+// 003. index.html, main.js 생성 및 <script defer type="moduele" src  > 추가
+// 004. package.js 파일 내 "script" 부분에 개발용, 빌드용 명령어 각각 추가
+// => "dev":"parcel ./index.html", "build":"parcel build ./index.html"
+
+//~원시형 데이터
+// 005-1. String 데이터
+//  -> const string1 = "hello" or 'hello' or `hello ${string2}`
+// 005-2. Number 데이터
+//  -> const number1 = 1 or -1 or 1.5, NaN(not a number)
+//  -> console.log(typeof(1+undefined)) // NaN
+//^     부동소수점오류 => console.log(0.1+0.2); // 0.30000000000000004
+//  -> toFixed()는 자리수를 잘라냄 => console.log((0.1+0.2).toFixed(2)); //0.30
+//  -> console.log(typeof((0.1+0.2).toFixed(2))); //~ string
+//  -> console.log(typeof(Number((0.1+0.2).toFixed(2)))); //~ number
+// 005-3. Boolean, null, undefined
+//  -> true or false
+//~참조형 데이터 
+// 006-1. Array 데이터
+//  -> const alphabet = ["a","b","c"]; console.log(alphabet[1]) //"b" => 인덱싱한다고 표현
+// 006-2 Object 데이터
+//  -> age:22 명칭 => key==속성==property : value==값
+//  -> 생성자 방식 const user = new Object(); user.name = "bora"; user.age=22 console.log(user) 
+//  -> //{name: 'bora', age: 22}
+//  -> function User() { this.name = "bora" this.age = 22 } const user = new User(); console.log(user);
+//  -> // User {name: 'bora', age: 22}
+//  -> 리터럴 방식 => 생성자 방식보다 간편하고 쉬움
